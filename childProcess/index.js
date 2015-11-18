7db3bebebe7759504036b7db417e0c6cae7dd270
goforbroke <koba.hige@gmail.com>
2015-11-18 11:14:16 +0900

    [FIX] (childProcess) index.js Fix path.

--- childProcess/index.js
+++ childProcess/index.js
@@ -1,7 +1,12 @@
+/**
+ * 子プロセスを起動するスクリプト
+ * @param  {[type]} function( [description]
+ * @return {[type]}           [description]
+ */
 (function() {
   var spawn = require('child_process').spawn;
-  var ssh    = spawn('ssh', ['-i', 'aws-rico.pem', 'centos@52.69.88.85', 'sudo', 'pwd; ls -l'],
-    {cwd :'/Users/koba/remote'});
+  var ssh    = spawn('ssh', ['localhost', 'pwd; ls -l'],
+    {cwd :'/tmp'});
 
   ssh.stdout.on('data', function (data) {
     console.log('stdout: ' + data);
