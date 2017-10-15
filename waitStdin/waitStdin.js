9d326ff23936e8de3c4a13dd0982ca57efcaeb0f
goforbroke <koba.hige@gmail.com>
2017-10-15 18:33:16 +0900

    [add]Wait stdin sample

---
+++ waitStdin/waitStdin.js
@@ -0,0 +1,23 @@
+// Node.js 8.6.0
+// Mac OS x Sierra
+// Node.jsはI/Oがノンブロッキングなため，Pythonのようにreadの読み出し完了を待ち受ける事ができない
+// そこで，標準入力の待ち受けが終わるまで入力をバッファに溜めて，まとめて処理を行う
+
+const lineReader = require('readline').createInterface({
+  input: process.stdin
+});
+
+// 1. 標準入力をEOF(Ctrl+D)が来るまで受け付ける
+let lines = [];
+lineReader.on('line', (line) => {
+  lines.push(line);
+});
+
+// 2. EOFをトリガに入力データの処理を行う
+// WORKAROUND: readline.on('end', )はEOFを検知しない
+process.stdin.on('close', () => {
+  const sets = Number(lines.shift());
+  for(let i = 0; i < sets; i++) {
+    console.log('Hello!, ', lines[i]);
+  }
+});
